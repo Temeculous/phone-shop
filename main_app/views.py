8f0655fbbc9c5f7f5e199429dcc1a654ea66a4fa
kezdetphia <fehermark88@gmail.com>
2023-07-07 17:42:53 +0200

    created the views function for the index.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+# Importing the phone model so we have acces to the class
+from .models import Phone           
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+# Accesses all the attributes of the phone class and passes the data down
+# to the index.html
+def phones_idex(request):
+    phones = Phone.objects.all()
+    return render(request, 'phones/index.html', {
+        'phones': phones
+    })
