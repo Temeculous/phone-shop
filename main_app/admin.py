3f766645b9882bd2613ca31353752d86c8a94c0b
kezdetphia <fehermark88@gmail.com>
2023-07-07 17:49:25 +0200

    created superuser, imported class to the admin and registered the class

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+
+# Importing phone class so we can register to the admin site
+from .models import Phone
 # Register your models here.
+admin.site.register(Phone)
