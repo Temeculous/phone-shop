a8e120acd9e10cbaf4b9766e51b0ef8c120b4d6c
kezdetphia <fehermark88@gmail.com>
2023-07-10 18:54:13 +0200

    heroku deployment

--- PHONE-SHOP/settings.py
+++ PHONE-SHOP/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
     'rest_framework'
 ]
 
