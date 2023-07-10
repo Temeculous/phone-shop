40268a712d58cefbe8d40b1ec874d235688bc677
kezdetphia <fehermark88@gmail.com>
2023-07-10 18:38:37 +0200

    heroku deployment

--- phoneShop/settings.py
+++ phoneShop/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',   #for heroku
 ]
 
 # for heroku
